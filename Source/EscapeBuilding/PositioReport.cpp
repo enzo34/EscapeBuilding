7a0e16b3263fe0f90410102e6eb625e1825f3256
enzo34 <eclavelloux@gmail.com>
2018-01-30 08:58:28 +0100

    Runtime messages for Feedback

--- Source/EscapeBuilding/PositioReport.cpp
+++ Source/EscapeBuilding/PositioReport.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "EscapeBuilding.h"
 #include "PositioReport.h"
+#include "EscapeBuilding.h"
+
 
 
 // Sets default values for this component's properties
@@ -21,8 +22,8 @@ void UPositioReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair !"));
+
 }
 
 
